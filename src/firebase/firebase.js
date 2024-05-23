ef1276a44c16edf85eab37091297a3aa55f2b6ef
UnknownRider <barikpriyam28@gmail.com>
2024-05-23 20:43:25 +0530

    Optimisation

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -5,13 +5,13 @@ import{getFirestore} from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBDDA8Jc3PDTF3omn032WWWvAS0CRH0hnA",
-  authDomain: "insta-clone-d1e19.firebaseapp.com",
-  projectId: "insta-clone-d1e19",
-  storageBucket: "insta-clone-d1e19.appspot.com",
-  messagingSenderId: "570321324716",
-  appId: "1:570321324716:web:84197ea872e206bdc2ed09",
-  measurementId: "G-BPBE1WTL05"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+	authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+	projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+	storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+	appId: import.meta.env.VITE_FIREBASE_APP_ID,
+	measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
